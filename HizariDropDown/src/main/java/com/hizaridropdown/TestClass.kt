2f33c16757b6ce05ee1e254a75cd65b5a2617294
Hizari <hhizari@gmail.com>
2023-03-01 15:10:45 +0700

    Setup lib

---
+++ HizariDropDown/src/main/java/com/hizaridropdown/TestClass.kt
@@ -0,0 +1,19 @@
+package com.hizaridropdown
+
+import android.util.Log
+
+/**
+ * hizari-ui - com.hizaridropdown
+ *
+ * Created by Hudio Hizari on 01/03/23.
+ * https://github.com/hudiohizari
+ *
+ */
+
+object TestClass {
+
+    fun test() {
+        Log.d("TestClass HDD", "test")
+    }
+
+}
---
+++ HizariEditText/src/main/java/com/hizariedittext/TestClass.kt
@@ -0,0 +1,19 @@
+package com.hizariedittext
+
+import android.util.Log
+
+/**
+ * hizari-ui - com.hizaridropdown
+ *
+ * Created by Hudio Hizari on 01/03/23.
+ * https://github.com/hudiohizari
+ *
+ */
+
+object TestClass {
+
+    fun test() {
+        Log.d("TestClass HET", "test")
+    }
+
+}
